15ce00754d183560311ef7ceb8a15b5405cccc2e
吕途 <buzhidaolvtu@163.com>
2018-10-21 17:57:46 +0800

    boot-plugin

--- sandbox-boot-maven-plugin/src/main/java/net/opensource/sandbox/boot/maven/RepackageMojo.java
+++ sandbox-boot-maven-plugin/src/main/java/net/opensource/sandbox/boot/maven/RepackageMojo.java
@@ -8,10 +8,15 @@ import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.MavenProjectHelper;
 
 import java.io.File;
+import java.net.URL;
+import java.util.Enumeration;
 import java.util.List;
 
 @Mojo(name = "repackage", defaultPhase = LifecyclePhase.PACKAGE, requiresProject = true, threadSafe = true, requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME, requiresDependencyCollection = ResolutionScope.COMPILE_PLUS_RUNTIME)
 public class RepackageMojo extends AbstractMojo {
+
+    private final static String SANBOX = "META-INF/sandbox/";
+    private final static String SANBOX_LOADER = "META-INF/sandbox-loader/";
 
     /**
      * The Maven project.
@@ -62,6 +67,32 @@ public class RepackageMojo extends AbstractMojo {
         }
         if (project.getPackaging().equals("jar")) {
             return;
+        }
+    }
+
+    private void repackage(){
+        try {
+            File sandbox_boot_agent = new File(target, "sandbox-boot-agent");
+            sandbox_boot_agent.delete();
+            sandbox_boot_agent.mkdir();
+
+            //解压sandbox-1.0.16-bin.zip
+            Enumeration<URL> sandboxResources = getClass().getClassLoader().getResources(SANBOX);
+            URL sandbox = sandboxResources.nextElement();
+            System.out.println(sandbox);
+            //解压sandbox-boot-loader-1.0-SNAPSHOT-jar-with-dependencies.jar
+            Enumeration<URL> sandboxLoaderResources = getClass().getClassLoader().getResources(SANBOX_LOADER);
+            URL sandboxLoader = sandboxLoaderResources.nextElement();
+            System.out.println(sandboxLoader);
+
+            //把user-module.jar放到user-module目录下面，如果是pom，就把子包的jar全部放到user-module下面
+            File user_modules_dir = new File(sandbox_boot_agent, "user-modules");
+            user_modules_dir.delete();
+            user_modules_dir.mkdir();
+
+            //把上面两部解压的结果再archive to jar package
+        }catch (Exception ex){
+            ex.printStackTrace();
         }
     }
 }
